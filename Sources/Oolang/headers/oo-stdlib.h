ca4c26e7ebc956c633c2ee3ffe99f1af6406b5a7
Johan Segerfeldt <johan.segerfeldt@me.com>
2025-10-28 05:03:59 +0100

    Remove unused print function

--- Sources/Oolang/headers/oo-stdlib.h
+++ Sources/Oolang/headers/oo-stdlib.h
@@ -7,17 +7,8 @@
 
 typedef uint64_t bitfield;
 
-static inline string* const bitfield_toString(uint64_t const value) {
-    return string_format("%018#" PRIx64, value);
-}
-
-/// @brief Implementation of bitfield.print()
-/// @param self the target bitfield “object”
-static inline void bitfield_print(bitfield self) {
-    // Print using "0x" prefix and 16 hex digits
-    string* description = bitfield_toString(self);
-    printf("%s\n", description->buffer);
-    free(description);
+static inline string* const bitfield_toString(bitfield const self) {
+    return string_format("%018#" PRIx64, self);
 }
 
 #endif /* OO_STDLIB_H */
